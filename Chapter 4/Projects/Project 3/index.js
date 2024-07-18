811bec3a297ede61b924480697d3df8d9bc1cc5c
Michael van den Berg <michaelmarkus115@gmail.com>
2024-07-18 08:33:17 +0200

    fourth commit

--- Chapter 4/Projects/Project 3/index.js
+++ Chapter 4/Projects/Project 3/index.js
@@ -1,29 +1,30 @@
 "use strict";
 
 function rockPaperScissors() {
-  const choices = ["Rock", "Paper", "Scissors"];
+  const choices = ["Rock", "Paper", "Scissors"]; //rps array
 
-  const playerIndex = Math.floor(Math.random() * 3);
-  const computerIndex = Math.floor(Math.random() * 3);
+  const playerIndex = Math.floor(Math.random() * 3); //choose random index
+  const computerIndex = Math.floor(Math.random() * 3); //choose random index
 
-  const playerChoice = choices[playerIndex];
+  const playerChoice = choices[playerIndex]; //assigns random index to element within the array
   const computerChoice = choices[computerIndex];
 
   let result;
 
+  //conditionals
   if (playerChoice === computerChoice) {
     result = "It's a tie!";
   } else if (
     (playerChoice === "Paper" && computerChoice === "Rock") ||
-    (playerChoice === "Scissors" && computerChoice === "Paper")
+    (playerChoice === "Scissors" && computerChoice === "Paper") ||
+    (playerChoice === "Rock" && computerChoice === "Scissors")
   ) {
     result = "You win!"; //if execute
-  } else if (playerChoice === "Rock" && computerChoice === "Scissors") {
-    result = "No one wins (execption)";
   } else {
     result = "Computer wins";
   }
 
+  //output
   console.log(`Player chose ${playerChoice}`);
   console.log(`Computer chose ${computerChoice}`);
   console.log(result);
